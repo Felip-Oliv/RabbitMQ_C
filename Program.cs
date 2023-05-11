a6e9bfd83e406e202cbb20f12ca239a56fb55108
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-05-11 20:38:44 -0300

    Add Program.cs
    Conexão e envio de mensagem

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Memama!");
+
+ConnectionFactory factory = new ConnectionFactory()
+{
+    HostName="localhost"
+
+};
+
+using(IConnection connection = factory.CreateConnection())
+{
+    using(IModel channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(
+            queue: "mensagens", 
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+
+        );
+        string texto = "mensagem teste";
+        byte[] mensagem = Encoding.UTF8.GetBytes(texto);
+        channel.BasicPublish(
+            body: mensagem,
+            routingKey: "mensagens",
+            basicProperties: null,
+            exchange: ""
+
+        );
+
+    }
+}
+Console.WriteLine("Mensagem enviada com sucessos");
+Console.ReadKey();
